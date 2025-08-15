1d26ad8d7424f58f2aa3d8244809d9edd371fdde
loxneshow2003 <ahamdhacky@hotmail.com>
2025-08-14 22:44:02 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bienvenue dans l'Outil d'Analyse des Revues de Code</h1>
+      <p>Analysez vos commentaires de revues de code et découvrez des statistiques intéressantes.</p>
+    </div>
+  );
+}
+
+export default App;
